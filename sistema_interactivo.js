6c51b5c4402ad1579253dc478304d18cccbee0f3
Envy528 <carmonaballestasj528@gmail.com>
2026-05-06 06:20:27 -0500

    W1 - Task 3: Age Validation

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,2 +1,8 @@
-let name = prompt("Enter your name");
-let age = prompt("Enter your age");
+// Variable declaration and user input
+const name = prompt("Enter your name");
+const age = Number(prompt("Enter your age"));
+
+// Check if the age is a valid number
+if (isNaN(age)) {
+    console.error("Error: Por favor, ingresa una edad válida en números.");
+}
