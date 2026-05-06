6c1d7b03cd8af8480a914dcb963b4ef5406ea502
Envy528 <carmonaballestasj528@gmail.com>
2026-05-06 08:07:47 -0500

    W1 - Task 4: Dynamic conditionals and messages

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -4,5 +4,16 @@ const age = Number(prompt("Enter your age"));
 
 // Check if the age is a valid number
 if (isNaN(age)) {
-    console.error("Error: Por favor, ingresa una edad válida en números.");
+    console.error("Error: Please, enter a valid age in numbers.");
+} else {
+    if (age < 0 || age > 120) {
+        alert("Error: Please, enter a realistic age between 0 and 120.");
+        console.error("Error: Please, enter a realistic age between 0 and 120.");
+    } else if (age < 18) {
+        alert(`Hello ${name}, you are a minor. Keep learning and enjoying coding!`);
+        console.log(`Hello ${name}, you are a minor. Keep learning and enjoying coding!`);
+    } else if (age >= 18){
+        alert(`Hello ${name}, you are an adult. Prepare for great opportunities in the world of programming!`);
+        console.log(`Hello ${name}, you are an adult. Prepare for great opportunities in the world of programming!`);
+    }
 }
