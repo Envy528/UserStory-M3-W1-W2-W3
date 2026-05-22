918b5e5328690582c1741b49aa1f9171eb2b0d16
Envy528 <carmonaballestasj528@gmail.com>
2026-05-21 23:41:26 -0500

    W2 - Task 2: Use of set in JavaScript

--- week 2/main.js
+++ week 2/main.js
@@ -1,4 +1,20 @@
-// Array de productos con objetos
-const productos = [{id:"1",nombre:"Televisor",precio:1500000},
-    {id:"2",nombre:"Celular",precio:80000},
-    {id:"3",nombre:"Computador",precio:2300000}]
+// Array with objects of products
+const products = [{id:"1",name:"Televisor",price:1500000},
+    {id:"2",name:"Celular",price:800000},
+    {id:"3",name:"Computador",price:2300000}]
+
+// Use of set in JavaScript
+const numbers = new Set([1,1,1,2,2,3,3,4,5,6,]);
+console.log(numbers, "Set erases duplicated numbers");
+
+numbers.add(7); // Method to add 7 to the numbers set
+console.log(numbers, "Added Number 7")
+
+console.log(numbers.has(1)); // Method to verify if 1 is in the numbers set
+
+numbers.delete(6); // Method to delete an item from the set
+console.log(numbers, "Number 6 is deleted");
+
+for (number of numbers){
+    console.log(number); // Prints every number in the set
+}
