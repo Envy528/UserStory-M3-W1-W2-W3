b90dd08392eea7cfe9f81d7106fe0d9e4c8095ef
Envy528 <carmonaballestasj528@gmail.com>
2026-05-22 02:00:24 -0500

    W2 - Task 4: Iteration on data structures

--- week 2/main.js
+++ week 2/main.js
@@ -1,8 +1,10 @@
+// ======================= Task 1 ====================
 // Array with objects of products
-const products = [{id:"1",name:"Desk",price:1500000, category:"Furniture"},
-    {id:"2",name:"Celphone",price:800000, category:"Electronics"},
-    {id:"3",name:"Computer",price:2300000, category:"Electronics"}]
+const products = [{id:1,name:"Desk",price:1500000, category:"Furniture"},
+    {id:2,name:"Celphone",price:800000, category:"Electronics"},
+    {id:3,name:"Computer",price:2300000, category:"Electronics"}]
 
+// ======================= Task 2 ======================
 // Use of set in JavaScript
 const numbers = new Set([1,1,1,2,2,3,3,4,5,6,]);
 console.log(numbers, "Set erases duplicated numbers");
@@ -19,6 +21,7 @@ for (const number of numbers){
     console.log(number); // Prints every number in the set
 }
 
+// =========================== Task 3 =======================
 // Creation of a Map
 const productsMap = new Map();
 
@@ -26,3 +29,22 @@ productsMap.set("Furniture", products[0].name);
 productsMap.set("Electronics", [products[1].name, products[2].name]); // products category and array of the products name so the map can store both products name
 
 console.log(productsMap, "Map created");
+
+// =============================== Task 4 =========================
+// Iteration on data structures
+//For in
+for (let i = 0; i < products.length; i++){ // First I take the indexes of the array
+    for (const value in products[i]){ // And here I iterate each product
+        console.log(`${value}: ${(products[i])[value]}`); // Shows the keys and it's values
+    }
+}
+
+//For of
+for (const number of numbers){ // Goes inside the Set and takes every item
+    console.log(number) // Shows every item
+}
+
+// ForEach
+productsMap.forEach((name, category) => { // Goes inside the Map and takes the category and the name of the products
+    console.log(`${category}: ${name}`); // Shows the category and the name of the products
+})
