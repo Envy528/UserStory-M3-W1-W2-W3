68e18642e11d2d76261c022a96bfc317adac9fd6
Envy528 <carmonaballestasj528@gmail.com>
2026-05-22 00:28:30 -0500

    W2 - Task 3: Map Creation

--- week 2/main.js
+++ week 2/main.js
@@ -1,7 +1,7 @@
 // Array with objects of products
-const products = [{id:"1",name:"Televisor",price:1500000},
-    {id:"2",name:"Celular",price:800000},
-    {id:"3",name:"Computador",price:2300000}]
+const products = [{id:"1",name:"Desk",price:1500000, category:"Furniture"},
+    {id:"2",name:"Celphone",price:800000, category:"Electronics"},
+    {id:"3",name:"Computer",price:2300000, category:"Electronics"}]
 
 // Use of set in JavaScript
 const numbers = new Set([1,1,1,2,2,3,3,4,5,6,]);
@@ -15,6 +15,14 @@ console.log(numbers.has(1)); // Method to verify if 1 is in the numbers set
 numbers.delete(6); // Method to delete an item from the set
 console.log(numbers, "Number 6 is deleted");
 
-for (number of numbers){
+for (const number of numbers){
     console.log(number); // Prints every number in the set
 }
+
+// Creation of a Map
+const productsMap = new Map();
+
+productsMap.set("Furniture", products[0].name);
+productsMap.set("Electronics", [products[1].name, products[2].name]); // products category and array of the products name so the map can store both products name
+
+console.log(productsMap, "Map created");
