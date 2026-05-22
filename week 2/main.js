9988638c02e8c14aca8cbe3af7b2766b141fa479
Envy528 <carmonaballestasj528@gmail.com>
2026-05-21 23:03:50 -0500

    W2 - Task 1: Creation of products object

---
+++ week 2/main.js
@@ -0,0 +1,4 @@
+// Array de productos con objetos
+const productos = [{id:"1",nombre:"Televisor",precio:1500000},
+    {id:"2",nombre:"Celular",precio:80000},
+    {id:"3",nombre:"Computador",precio:2300000}]
