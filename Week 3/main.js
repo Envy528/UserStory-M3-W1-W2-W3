14720f12cb99a4c04f71ea8a74e2dc2365f8954e
Envy528 <carmonaballestasj528@gmail.com>
2026-05-27 07:16:02 -0500

    W3 - Task 2: Selection and inspection (JS)

---
+++ Week 3/main.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("agregarNota");
+const input = document.getElementById("nota");
+const salida = document.getElementById("listaNotas");
+
+console.log(btn, input, salida)
